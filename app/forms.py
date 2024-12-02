018045d7b616bf39d663f76b5799f9d39a1232c6
George Doyle <gdoyle046@gmail.com>
2024-12-02 10:37:44 +0000

    Allow 0 value for SNP/exon padding

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SubmitField, SelectField, TextAreaField, FileField, BooleanField
-from wtforms.validators import DataRequired, NumberRange, Optional
+from wtforms.validators import DataRequired, NumberRange, Optional, InputRequired
 
 class SettingsForm(FlaskForm):
-    data_padding = IntegerField('Data Padding', validators=[DataRequired(), NumberRange(min=0)], default=0)
-    sambamba_padding = IntegerField('Sambamba Padding', validators=[DataRequired(), NumberRange(min=0)], default=0)
-    exomeDepth_padding = IntegerField('ExomeDepth Padding', validators=[DataRequired(), NumberRange(min=0)], default=0)
-    cnv_padding = IntegerField('CNV Padding', validators=[DataRequired(), NumberRange(min=0)], default=0)
-    data_snp_padding = IntegerField('Data SNP Padding', validators=[DataRequired(), NumberRange(min=0)], default=0)
-    sambamba_snp_padding = IntegerField('Sambamba SNP Padding', validators=[DataRequired(), NumberRange(min=0)], default=0)
-    exomeDepth_snp_padding = IntegerField('ExomeDepth SNP Padding', validators=[DataRequired(), NumberRange(min=0)], default=0)
-    cnv_snp_padding = IntegerField('CNV SNP Padding', validators=[DataRequired(), NumberRange(min=0)], default=0)
+    data_padding = IntegerField('Data Padding', validators=[InputRequired(), NumberRange(min=0, message="Padding must be 0 or greater")], default=0)
+    sambamba_padding = IntegerField('Sambamba Padding', validators=[InputRequired(), NumberRange(min=0, message="Padding must be 0 or greater")], default=0)
+    exomeDepth_padding = IntegerField('ExomeDepth Padding', validators=[InputRequired(), NumberRange(min=0, message="Padding must be 0 or greater")], default=0)
+    cnv_padding = IntegerField('CNV Padding', validators=[InputRequired(), NumberRange(min=0, message="Padding must be 0 or greater")], default=0)
+    data_snp_padding = IntegerField('Data SNP Padding', validators=[InputRequired(), NumberRange(min=0, message="SNP padding must be 0 or greater")], default=0)
+    sambamba_snp_padding = IntegerField('Sambamba SNP Padding', validators=[InputRequired(), NumberRange(min=0, message="SNP padding must be 0 or greater")], default=0)
+    exomeDepth_snp_padding = IntegerField('ExomeDepth SNP Padding', validators=[InputRequired(), NumberRange(min=0, message="SNP padding must be 0 or greater")], default=0)
+    cnv_snp_padding = IntegerField('CNV SNP Padding', validators=[InputRequired(), NumberRange(min=0, message="SNP padding must be 0 or greater")], default=0)
     data_include_5utr = BooleanField("Include 5' UTR for Data BED")
     data_include_3utr = BooleanField("Include 3' UTR for Data BED")
     sambamba_include_5utr = BooleanField("Include 5' UTR for Sambamba BED")
